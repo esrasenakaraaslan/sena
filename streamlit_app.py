da42f4b692ddf760901ac9505a6752f891239eda
esrasenakaraaslan <147700240+esrasenakaraaslan@users.noreply.github.com>
2024-06-06 16:02:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,28 @@ from io import BytesIO
 # Uygulama ayarları
 st.set_page_config(page_title="FreshData", page_icon=":rocket:", layout="wide")
 
+# CSS stil tanımları
+st.markdown(
+    """
+    <style>
+        /* Arka plan rengi */
+        body {
+            background-color: #3498db; /* Mavi renk */
+        }
+        /* Butonların rengi */
+        .stButton>button {
+            background-color: #9b59b6; /* Mor renk */
+            color: #f4d03f; /* Sarı renk */
+        }
+        /* Butonların üstündeki metin rengi */
+        .stButton>button div div {
+            color: #f4d03f; /* Sarı renk */
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Başlık
 st.markdown('<h1 style="color: #9b59b6; text-align: center;">FreshData İş İlanı Sitesi</h1>', unsafe_allow_html=True)
 
@@ -43,51 +65,39 @@ if st.button("Analiz"):
     st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><p style="color: #f4d03f;">Burada veri analizi işlevi gelecek.</p></div>', unsafe_allow_html=True)
 
 if st.button("Grafikler"):
-    st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><p style="color: #f4d03f;">Projemizdeki grafiklerimiz:</p></div>', unsafe_allow_html=True)
+    st.write("Grafikler")
     
     # Konum Grafiği
     st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><p style="color: #f4d03f;">Konum sütununda en çok tekrar eden 5 konum</p></div>', unsafe_allow_html=True)
-    # Çubuk grafiği çizme işlevi
     top_locations = df['Konum'].value_counts().head(5)
     st.bar_chart(top_locations)
 
     # Çalışma Şekli Grafiği
     st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><p style="color: #f4d03f;">Çalışma şekli sütunu</p></div>', unsafe_allow_html=True)
-    # Çubuk grafiği çizme işlevi
     calisma_sekli_sayilari = df['çalışma şekli'].value_counts()
     st.bar_chart(calisma_sekli_sayilari)
-  
+
     # Pozisyon Grafiği
     st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><p style="color: #f4d03f;">Pozisyon sütununda en çok tekrar eden 20 pozisyon</p></div>', unsafe_allow_html=True)
-    # Çubuk grafiği çizme işlevi
     top_positions = df['Pozisyon'].value_counts().head(20)
     st.bar_chart(top_positions)
 
-    # En çok tekrar eden konumlar ve pozisyonlar
-en_cok_tekrar_edilen_konumlar = df['Konum'].value_counts().head(10).index
-en_cok_tekrar_edilen_pozisyonlar = df['Pozisyon'].value_counts().head(10).index
-
-# Filtrelenmiş veri
-filtrelenmis_veri = df[
-    df['Konum'].isin(en_cok_tekrar_edilen_konumlar) & 
-    df['Pozisyon'].isin(en_cok_tekrar_edilen_pozisyonlar)
-]
-
-# Çapraz tablo oluştur
-cross_table = pd.crosstab(index=filtrelenmis_veri['Konum'], columns=filtrelenmis_veri['Pozisyon'])
-
-# Streamlit ile görselleştirme
-st.write("En Çok Tekrar Edilen İlk 10 Konum ve Pozisyon İlişkisi Heatmap")
-plt.figure(figsize=(10, 6))
-sns.heatmap(cross_table, cmap='rainbow', annot=True, fmt='d', linewidths=.5)
-plt.title('En Çok Tekrar Edilen İlk 10 Konum ve Pozisyon İlişkisi Heatmap')
-plt.xlabel('Pozisyon')
-plt.ylabel('Konum')
-st.pyplot(plt)
-
+    # Çalışma Şekli ve Pozisyon Grafiği
+    st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><p style="color: #f4d03f;">Çalışma şekli ve pozisyon sütunlarında en çok tekrar eden 5 pozisyonun çalışma şekillerine göre dağılımı</p></div>', unsafe_allow_html=True)
+    top_5_positions = df['Pozisyon'].value_counts().head(5).index
+    filtered_data = df[df['Pozisyon'].isin(top_5_positions)]
+    plt.figure(figsize=(10, 6))
+    sns.countplot(data=filtered_data, x='Pozisyon', hue='çalışma şekli', palette='viridis')
+    plt.title('En Çok Tekrar Eden 5 Pozisyonun Çalışma Şekillerine Göre Dağılımı')
+    plt.xlabel('Pozisyon')
+    plt.ylabel('Sayı')
+    st.pyplot(plt)
 
 if st.button("İşveren Girişi", key="isveren_girisi_button"):
     st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><p style="color: #f4d03f;">Burada işveren giriş işlevi gelecek.</p></div>', unsafe_allow_html=True)
+
+if st.button("Regresyon"):
+    st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><p style="color: #f4d03f;">Burada regresyon analizi yapılacak.</p></div>', unsafe_allow_html=True)
 
 # Makaleler bölümü
 st.markdown('<h2 style="color: #9b59b6; text-align: center;">Makaleler</h2>', unsafe_allow_html=True)
