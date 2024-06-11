33a7cbb7a051eed49a274963d2293383bd6ba074
esrasenakaraaslan <147700240+esrasenakaraaslan@users.noreply.github.com>
2024-06-11 13:55:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -206,13 +206,7 @@ if st.button("Tahmin Et!!"):
     else:
         st.error("Yanlış Tahmin!")
         
-# Eğitim ve test verisi doğruluk puanları
-if st.button("Eğitim ve Test Verisi Doğruluk Puanı:"):
-    st.markdown('<div style="color: red;">Eğitim verisi doğruluk puanı:</div>', unsafe_allow_html=True)
-    st.markdown(f'<div style="color: red;">{train_accuracy}</div>', unsafe_allow_html=True)
-    st.markdown('<div style="color: red;">Test verisi doğruluk puanı:</div>', unsafe_allow_html=True)
-    st.markdown(f'<div style="color: red;">{test_accuracy}</div>', unsafe_allow_html=True) 
-    
+
 # Meslek Grupları butonunun durumunu takip eden bir oturum durumu (session state) belirle
 if 'meslek_gruplari_acik' not in st.session_state:
     st.session_state.meslek_gruplari_acik = False
